031fbcbd7fab70fbf5c5cf706ccde512ada03dd0
Infinidoge <infinidoge@doge-inc.net>
2022-08-28 00:25:05 -0400

    refactor(minecraft-servers): remove jq from update.py

--- pkgs/minecraft-servers/update.py
+++ pkgs/minecraft-servers/update.py
@@ -1,12 +1,11 @@
 #!/usr/bin/env nix-shell
-#!nix-shell -i python3 -p python3Packages.requests python3Packages.jq
+#!nix-shell -i python3 -p python3Packages.requests
 
 import json
+import requests
 from pathlib import Path
 from typing import Union, Dict
 
-import jq
-import requests
 
 # These versions don't have servers
 BLACKLIST = [
@@ -18,26 +17,11 @@ BLACKLIST = [
     "1.0",
 ]
 
-# Transforms the version manifest into a dict of {id: url}
-MANIFEST_PARSER = jq.compile(
-    ".versions "
-    '| map(select(.type == "release" or .type == "snapshot")) '  # Ignore alpha/beta versions
-    "| [.[] | {key:.id,value:.url}] | from_entries"  # Convert to {id: url}
-)
-
-# Transforms the version json into the lock format {url:str, sha1:str, version:str, javaVersion:int}
-# If the java version isn't present, defaults to 8
-VERSION_PARSER = jq.compile(
-    "if .downloads.server "
-    "then {url:.downloads.server.url, sha1:.downloads.server.sha1, version:.id, javaVersion:(.javaVersion.majorVersion // 8)} "
-    "else null end"
-)
-
 
 def parse_manifest() -> Dict[str, str]:
     """
-    Fetches the version manifest from Mojang and runs it through the jq process MANIFEST_PARSER
-    Returns its output as {id: url}
+    Fetches the version manifest from Mojang and processes it
+    Returns its output as a dict of {id: url}
     """
 
     print("Fetching manifest")
@@ -45,26 +29,44 @@ def parse_manifest() -> Dict[str, str]:
         "https://launchermeta.mojang.com/mc/game/version_manifest.json"
     )
     response.raise_for_status()
-    return MANIFEST_PARSER.input(response.json()).first()
+
+    return dict(
+        map(
+            lambda elem: (elem["id"], elem["url"]),
+            filter(
+                lambda elem: elem["type"] in ("release", "snapshot"),
+                response.json()["versions"],
+            ),
+        )
+    )
 
 
-def parse_version(url) -> Dict[str, Union[str, int]]:
+def parse_version(url) -> Union[Dict[str, Union[str, int]], None]:
     """
-    Fetches the version JSON at the URl and runs it through the jq process VERSION_PARSER
+    Fetches the version JSON at the URl and processes it
     Returns a dict in the form:
     {
         "url": string,
         "sha1": string,
         "version": string,
-        javaVersion: int,
-        manifestUrl: string
+        "javaVersion": int,
+        "manifestUrl": string
     }
     """
 
     print(f"Fetching {url}")
     response = requests.get(url)
     response.raise_for_status()
-    return VERSION_PARSER.input(response.json()).first() | {"manifestUrl": url}
+
+    data = response.json()
+    if "server" in data["downloads"]:
+        return {
+            "url": data["downloads"]["server"]["url"],
+            "sha1": data["downloads"]["server"]["sha1"],
+            "version": data["id"],
+            "javaVersion": data.get("javaVersion", {"majorVersion": 8})["majorVersion"],
+            "manifestUrl": url,
+        }
 
 
 def main(versions, lock_file):
