15adeec673ee4cce57d66efead2335dd099a423f
Gabriel Fontes <hi@m7.rs>
2023-01-07 17:41:06 -0300

    feat: package velocity

---
+++ pkgs/velocity-servers/default.nix
@@ -0,0 +1,29 @@
+{ callPackage
+, lib
+}:
+let
+  inherit (builtins) listToAttrs sort filter;
+  inherit (lib) nameValuePair last recurseIntoAttrs flatten mapAttrsToList versionOlder importJSON;
+  inherit (lib.our) escapeVersion;
+  versions = importJSON ./lock.json;
+
+  # Sort by attribute 'attr' using 'f' function
+  sortBy = attr: f: sort (a: b: f a.${attr} b.${attr});
+
+  packages =
+    mapAttrsToList
+    (version: builds:
+      sortBy "version" versionOlder (mapAttrsToList
+        (buildNumber: value:
+          callPackage ./derivation.nix {
+            inherit (value) url sha256 channel;
+            version = "${version}-build.${buildNumber}";
+          })
+        builds))
+    versions;
+  stablePackages = map (filter (x: x.meta.branch != "experimental")) packages;
+in
+  recurseIntoAttrs (listToAttrs (
+    (map (x: nameValuePair (escapeVersion x.name) x) (flatten packages))
+    ++ [(nameValuePair "velocity" (last (last stablePackages)))]
+  ))
---
+++ pkgs/velocity-servers/derivation.nix
@@ -0,0 +1,35 @@
+{ lib, stdenv, fetchurl, nixosTests, jre_headless, version, url, sha256, channel ? "default" }:
+stdenv.mkDerivation {
+  pname = "velocity";
+  inherit version;
+
+  src = fetchurl { inherit url sha256; };
+
+  preferLocalBuild = true;
+
+  installPhase = ''
+    mkdir -p $out/bin $out/lib/minecraft
+    cp -v $src $out/lib/minecraft/server.jar
+    cat > $out/bin/velocity << EOF
+    #!/bin/sh
+    exec ${jre_headless}/bin/java \$@ -jar $out/lib/minecraft/server.jar nogui
+    EOF
+    chmod +x $out/bin/velocity
+  '';
+
+  dontUnpack = true;
+
+  passthru = {
+    tests = { inherit (nixosTests) minecraft-server; };
+    updateScript = ./update.py;
+  };
+
+  meta = with lib; {
+    description = "A modern, next-generation Minecraft server proxy";
+    homepage = "https://papermc.io";
+    license = licenses.gpl3Only;
+    platforms = platforms.unix;
+    maintainers = with maintainers; [ misterio77 ];
+    branch = channel;
+  };
+}
