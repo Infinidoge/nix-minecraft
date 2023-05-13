00e9e1d4e2f6a166be74949e3d435c9cb76e01b7
Infinidoge <infinidoge@inx.moe>
2023-05-13 15:57:20 -0400

    flake: add flake-compat

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = { url = "github:edolstra/flake-compat"; flake = false; };
   };
 
   outputs =
