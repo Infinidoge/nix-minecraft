0b4ba81e5ce346ceb3a5aebbc9295c2f7e482ef4
Gabriel Fontes <hi@m7.rs>
2023-10-24 12:00:32 -0300

    tests: init (#38)

---
+++ tests/files-symlinks.nix
@@ -0,0 +1,53 @@
+{ nixosTest, outputs }:
+
+nixosTest {
+  name = "files-symlinks";
+  nodes.server = { config, pkgs, lib, ...}: {
+    nixpkgs = {
+      overlays = [ outputs.overlays.default ];
+      config.allowUnfree = true;
+    };
+    imports = [ outputs.nixosModules.minecraft-servers ];
+
+    services.minecraft-servers = {
+      enable = true;
+      eula = true;
+      servers.paper = {
+        enable = true;
+        jvmOpts = "-Xmx512M"; # Avoid OOM
+        package = pkgs.paperServers.paper-1_19_4;
+        serverProperties = {
+          server-port = 25565;
+          level-type = "flat"; # Make the test lighter
+          online-mode = false;
+        };
+        symlinks = {
+          # To avoid internet access
+          "cache/mojang_1.19.4.jar" = "${pkgs.vanillaServers.vanilla-1_19_4}/lib/minecraft/server.jar";
+        };
+        files = {
+          # A mutable file
+          "ops.json".value = [{
+            name = "Misterio7x";
+            # Offline mode UUID that is derived from the username
+            # Does not need internet to resolve, and never changes.
+            uuid = "b094c46b-90d6-385a-96d9-5e740ed98070";
+            level = 4;
+          }];
+          "spigot.yml".value = {
+            messages.unknown-command = "Unknown command, dummy!";
+          };
+        };
+      };
+    };
+  };
+
+  testScript = { nodes, ... }: ''
+    name = "paper"
+    grep_logs = lambda expr: f"grep '{expr}' /srv/minecraft/{name}/logs/latest.log"
+
+    server.wait_for_unit(f"minecraft-server-{name}.service")
+    server.wait_for_open_port(25565)
+    server.wait_until_succeeds(grep_logs("Done ([0-9.]\+s)! For help, type \"help\""), timeout=30)
+  '';
+}
