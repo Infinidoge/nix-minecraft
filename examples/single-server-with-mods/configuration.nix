59f322ad9e5c3185a1b03e13e9be5e82e4830017
Aaron Honeycutt <aaronhoneycutt@protonmail.com>
2025-02-01 17:14:14 -0700

    Add example with mods: clean up single server example without mods

---
+++ examples/single-server-with-mods/configuration.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, lib, ... }:
+
+{
+  # Minecraft server settings
+  services.minecraft-servers = {
+    enable = true;
+    eula = true;
+    openFirewall = true;
+    servers.fabric = {
+       enable = true;
+       jvmOpts = "-Xmx4G -Xms2G";
+
+       # Specify the custom minecraft server package
+       package = pkgs.fabricServers.fabric-1_21_1.override { loaderVersion = "0.16.10"; }; # Specific fabric loader version
+
+       symlinks = {
+          mods = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
+            Fabric-API = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/P7dR8mSH/versions/9YVrKY0Z/fabric-api-0.115.0%2B1.21.1.jar"; sha512 = "e5f3c3431b96b281300dd118ee523379ff6a774c0e864eab8d159af32e5425c915f8664b1"; };
+            Backpacks = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/MGcd6kTf/versions/Ci0F49X1/1.2.1-backpacks_mod-1.21.2-1.21.3.jar"; sha512 = "6efcff5ded172d469ddf2bb16441b6c8de5337cc623b6cb579e975cf187af0b79291"; };
+          });
+       };
+     };
+  };
+
+}
--- examples/single-server/configuration.nix
+++ examples/single-server/configuration.nix
@@ -11,13 +11,8 @@
        jvmOpts = "-Xmx4G -Xms2G";
 
        # Specify the custom minecraft server package
-       package = pkgs.fabricServers.fabric-1_20_1.override { loaderVersion = "0.16.9"; }; # Specific fabric loader version
+       package = pkgs.fabricServers.fabric-1_21_1.override { loaderVersion = "0.16.10"; }; # Specific fabric loader version
 
-       symlinks = {
-          mods = pkgs.linkFarmFromDrvs "mods" (builtins.attrValues {
-            BetterMC = pkgs.fetchurl { url = "https://cdn.modrinth.com/data/shFhR8Vx/versions/Ur9uoHH5/Better%20MC%20%5BFABRIC%5D%20-%20BMC2%20v26.5.mrpack"; sha512 = "014f93917b238267ccf0b1644fdef722f2fbc18ce78fe28012d8932612b5ac0573b3f93fa7a990cf2516071e799856291805f2114b3e2cf7330c7eb22b77f1f3";>
-          });
-       };
      };
   };
 
