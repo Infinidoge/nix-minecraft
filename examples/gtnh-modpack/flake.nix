84c7cb36133e138c20fb23434a08f7e26040e10e
Gabriel Fontes <hi@m7.rs>
2025-05-28 17:36:56 +0200

    feat(templates): init velocity-network and gtnh-modpack

---
+++ examples/gtnh-modpack/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Homelab running Minecraft";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nix-minecraft.url = "github:Infinidoge/nix-minecraft";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      nix-minecraft,
+      ...
+    }:
+    {
+      nixosConfigurations."edi" = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          nix-minecraft.nixosModules.minecraft-servers
+          {
+            nixpkgs.overlays = [ inputs.nix-minecraft.overlay ];
+          }
+        ];
+      };
+    };
+}
