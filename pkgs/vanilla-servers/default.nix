3afef3cfa4f0b946afd4aee39d42de5e951a2cc4
Infinidoge <infinidoge@inx.moe>
2026-04-09 11:52:11 -0400

    vanilla-servers: use jdk25 as latest jdk

--- pkgs/vanilla-servers/default.nix
+++ pkgs/vanilla-servers/default.nix
@@ -1,8 +1,8 @@
 {
   callPackage,
   lib,
-  jre8,
-  jre_headless,
+  jdk8,
+  jdk25,
 }:
 let
   versions = lib.importJSON ./versions.json;
@@ -10,9 +10,9 @@ let
   # Older Minecraft versions that were written for Java 8, required Java 8.
   # Mojang has since rewritten a lot of their codebase so that Java versions
   # are no longer as important for stability as they used to be. Meaning we can
-  # target latest the latest JDK for all newer versions of Minecraft.
+  # target the latest JDK for all newer versions of Minecraft.
   # TODO: Assert that jre_headless >= java version
-  getJavaVersion = v: if v == 8 then jre8 else jre_headless;
+  getJavaVersion = v: if v == 8 then jdk8 else jdk25;
 
   packages = lib.mapAttrs' (version: value: {
     name = "vanilla-${lib.our.escapeVersion version}";
